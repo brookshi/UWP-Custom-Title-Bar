ae84495a6dea5d78b1bbbeec9825a06f4ea5ca7c
brookshi <iwxiaot@gmail.com>
2015-09-03 21:41:05 +0800

    nuget

--- CustomTitleBar/CustomTitleBar.xaml.cs
+++ CustomTitleBar/CustomTitleBar.xaml.cs
@@ -1,24 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
+﻿using System.ComponentModel;
 using Windows.ApplicationModel.Core;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.UI;
 using Windows.UI.ViewManagement;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Markup;
 using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 
-namespace CustomTitleBar
+namespace CN.Brook.UI
 {
     [ContentProperty(Name = "TitleBarControl")]
     public sealed partial class CustomTitleBar : UserControl, INotifyPropertyChanged
